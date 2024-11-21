8d948998c1e95510538acc60ded415c2b80104d0
chan8632 <duscks8632@gmail.com>
2024-11-21 11:46:47 +0900

    Location의 메소드, Location으로 받아온 정보들 useState로 처리하기, 온도, 날씨를 map를 통해서 띄우기

--- App.js
+++ App.js
@@ -1,34 +1,102 @@
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({
+      latitude,
+      longitude,
+    });
+    setCity(location[0].city);
+    setTimeout(() => {
+      setDays([
+        {
+          temp: { day: 19.49 },
+          weather: [{ main: "Clouds", description: "broken clouds" }],
+        },
+        {
+          temp: { day: 22.03 },
+          weather: [{ main: "Clear", description: "clear sky" }],
+        },
+        {
+          temp: { day: 19.92 },
+          weather: [{ main: "Clouds", description: "few clouds" }],
+        },
+        {
+          temp: { day: 19.39 },
+          weather: [{ main: "Clouds", description: "overcast clouds" }],
+        },
+        {
+          temp: { day: 19.7 },
+          weather: [{ main: "Clouds", description: "few clouds" }],
+        },
+        {
+          temp: { day: 17.19 },
+          weather: [{ main: "Clouds", description: "broken clouds" }],
+        },
+        {
+          temp: { day: 17.32 },
+          weather: [{ main: "Rain", description: "light rain" }],
+        },
+        {
+          temp: { day: 18.04 },
+          weather: [{ main: "Clouds", description: "few clouds" }],
+        },
+      ]);
+    }, 2000);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
         pagingEnabled
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -63,5 +131,8 @@ const styles = StyleSheet.create({
   description: {
     marginTop: -30,
     fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });
